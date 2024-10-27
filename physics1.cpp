c4b9aac84dbdb5ab24b0056e85db0e8abf793e35
TheHimanshu08 <himpandey2006@gmail.com>
2024-10-27 18:52:56 +0530

    Update physics1.cpp

--- physics1.cpp
+++ physics1.cpp
@@ -1,14 +1,15 @@
 #include<stdio.h>
-// #include<conio.h>
+#include<conio.h>
 
 void main() {
-    // clrscr();
-    float u, a, t, V, S;
-    printf("Enter initial velocity (u), acceleration (a), and time (t): ");
+    
+    float u, a, t, v, s;
+    clrscr();
+    printf("Enter u, a ,t  ");
     scanf("%f%f%f", &u, &a, &t);
-    V = u + a * t;
-    S = u * t + 0.5 * a * t * t;
-    printf("V = %.2f\n", V);
-    printf("S = %.2f", S);
-    // getch();
+    v = u + a * t;
+    s = u * t + 0.5 * a * t * t;
+    printf("v = %f", v);
+    printf("s = %f", s);
+    getch();
 }
